704f4f91d1ae754420c4969a4936f4b6b7f57b91
mrivs <valentinilyins@gmail.com>
2022-11-11 23:09:54 +0300

    task 05 mod 01

--- task05/Program.cs
+++ task05/Program.cs
@@ -7,101 +7,85 @@
 10 09 08 07
 /*/
 
-int[,] matrix = CreateMatrix(3, 7);
+int[,] matrix = CreateMatrix(4, 4);
+SetSpiral(matrix);
+PrintMatrix(matrix);
 
-int length = matrix.GetLength(1);
-int height = matrix.GetLength(0);
-
-int[] position = { 0, 0 };
-int hor = length - 1;
-int ver = height - 1;
-
-int count = hor*ver; 
-
-int value = 01;
-
-matrix[0, 0] = value;
-
-value++;
-
-for (int i = 0; i < hor; i++)
+void SetSpiral(int[,] matrix)
 {
-    MoveRight(position);
-    matrix[position[0], position[1]] = value;
-    value++;
-}
-for (int i = 0; i < ver; i++)
-{
-    MoveDown(position);
-    matrix[position[0], position[1]] = value;
-    value++;
+    int[] position = { 0, 0 };
+    int hor = matrix.GetLength(1);
+    int ver = matrix.GetLength(0); 
+    int count = hor * ver;
+    int value = 01;
     
-}
-ver--;
-
-while (count > 0)
-{  
-
-    for (int i = 0; i < hor; i++)
-    {
-        MoveLeft(position);
-        matrix[position[0], position[1]] = value;
-        value++;
-        count--;
-    }
-    hor--;
-    if (count<1) break;
-    for (int i = 0; i < ver; i++)
-    {
-        MoveUp(position);
-        matrix[position[0], position[1]] = value;
-        value++;
-        count--;
-    }
-    ver--;
-    if (count<1) break;
     for (int i = 0; i < hor; i++)
     {
         MoveRight(position);
         matrix[position[0], position[1]] = value;
         value++;
-        count--;
     }
-    hor--;
-    if (count<1) break;
     for (int i = 0; i < ver; i++)
     {
         MoveDown(position);
         matrix[position[0], position[1]] = value;
         value++;
-        count--;
     }
     ver--;
-    if (count<1) break;
-}
-PrintMatrix(matrix);
-void MoveRight(int[] position)
-{
-    position[1]++;
-}
-void MoveLeft(int[] position)
-{
-    position[1]--;
-}
-void MoveUp(int[] position)
-{
-    position[0]--;
-}
-void MoveDown(int[] position)
-{
-    position[0]++;
-}
-void LineRight(int[] position, int steps, int value, int[,] matrix)
-{
-    for (int i = 0; i < steps; i++)
+
+    while (count > 0)
+    {
+        for (int i = 0; i < hor; i++)
+        {
+            MoveLeft(position);
+            SetValue();
+        }
+        hor--;
+        if (count < 1) break;
+        for (int i = 0; i < ver; i++)
+        {
+            MoveUp(position);
+            SetValue();
+        }
+        ver--;
+        if (count < 1) break;
+        for (int i = 0; i < hor; i++)
+        {
+            MoveRight(position);
+            SetValue();
+        }
+        hor--;
+        if (count < 1) break;
+        for (int i = 0; i < ver; i++)
+        {
+            MoveDown(position);
+            SetValue();
+        }
+        ver--;
+        if (count < 1) break;
+    }
+    void SetValue()
     {
         matrix[position[0], position[1]] = value;
         value++;
+        count--;
+    }
+
+    void MoveRight(int[] position)
+    {
+        position[1]++;
+    }
+    void MoveLeft(int[] position)
+    {
+        position[1]--;
+    }
+    void MoveUp(int[] position)
+    {
+        position[0]--;
+    }
+    void MoveDown(int[] position)
+    {
+        position[0]++;
     }
 }
 
@@ -127,8 +111,7 @@ void PrintMatrix(int[,] matrix) // отображаем матрицу
     {
         for (int col = 0; col < matrix.GetLength(1); col++)
         {
-            //Console.Write($"{matrix[row, col]}  ");
-           Console.Write( string.Format("{0:d2}  ", matrix[row, col]));
+            Console.Write(string.Format("{0:d2} ", matrix[row, col]));
         }
         Console.WriteLine();
     }
